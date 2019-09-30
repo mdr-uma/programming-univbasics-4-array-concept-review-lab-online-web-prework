1999aadd5a4857f385d887764d40fc530993b73a
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-09-30 03:49:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,8 +12,7 @@ end
 
 def find_max_value(array)
   x = 0
-  array.length.times {|index| x = array[index]
-  if array[index] > x}
+  array.length.times { |index| x = array[index] if array[index] > x }
     x
 end
 
